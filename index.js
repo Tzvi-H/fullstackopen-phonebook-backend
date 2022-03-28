a0aedc790fe6da5115664713a38561bad7ec04d0
Tzvi-H <16guitar@gmail.com>
2022-03-28 16:15:00 -0500

    3.3 done

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 })

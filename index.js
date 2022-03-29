6d894bf2b6191dd4b8d0cbe6b162906a9ac6e4b9
Tzvi-H <16guitar@gmail.com>
2022-03-29 12:19:58 -0500

    3.9 done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 let persons = require('./db.json')
 const PORT = 3001;
 const app = express();
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
